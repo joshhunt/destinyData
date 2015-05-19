cd878f494a11f59ab3efe48fa1d5781747f0cb9e
Josh Hunt <josh@trtr.co>
2015-05-19 23:22:56 +1000

    Update upload script

--- upload.sh
+++ upload.sh
@@ -5,9 +5,7 @@ archive=$( base64 archiveLambda.zip )
 dir=$(pwd)
 
 echo ''
-echo 'Uploading new archive from $dir'
-aws lambda update-function-code --region us-east-1 --function-name myFirstFunction --zip-file fileb://${dir}/archiveLambda.zip
+echo 'Uploading new archive...'
+aws lambda update-function-code --region us-east-1 --function-name destinyPlumbing --zip-file fileb://${dir}/archiveLambda.zip
 
-echo ''
-echo 'Executing'
-aws lambda invoke --region us-east-1 --invocation-type RequestResponse --function-name myFirstFunction --log-type Tail --payload '{}' output.txt
+echo 'Done. Lambda function ready to be executed.'
